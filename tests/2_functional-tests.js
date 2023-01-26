231d166579d1e7ed967c0e61b188d6f13011c43e
re-spawn <rob@robanthony.ca>
2023-01-26 09:42:28 -0330

    chore: setup

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,7 +1,14 @@
-const chai = require("chai");
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
-const server = require('../server');
+
+// const server = require('../server');
+require('dotenv').config();
+if (process.env.HOST === 'repl') {
+  var server = 'https://fCC-07-04-sudoku-solver.respawn709.repl.co';
+} else {
+  var server = require('../server');
+}
 
 chai.use(chaiHttp);
 
