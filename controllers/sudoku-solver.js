992271e11db0eef272334d2ecf2f27b43009de59
re-spawn <rob@robanthony.ca>
2023-01-27 22:08:55 -0330

    feat: solver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,23 +1,151 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (puzzleString.search(/[^1-9\.]/) != -1) {
+      return {
+        valid: false,
+        error: 'Invalid characters in puzzle'
+      };
+    }
+    if (puzzleString.length != 81) {
+      return {
+        valid: false,
+        error: 'Expected puzzle to be 81 characters long'
+      };
+    }
+    return {
+      valid: true
+    };
+  }
+
+  insertPlaceholderIntoPuzzleString(puzzleString, row, column) {
+    return (
+      puzzleString.slice(0, (row - 1) * 9 + (column - 1)) +
+      '.' +
+      puzzleString.slice((row - 1) * 9 + (column - 1) + 1)
+    );
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    const puzzleStringWithPlaceholder =
+      this.insertPlaceholderIntoPuzzleString(puzzleString, row, column);
+    const puzzleRowWithPlaceholder =
+      [...(puzzleStringWithPlaceholder.substr((row - 1) * 9, 9))];
+    if (puzzleRowWithPlaceholder.includes(value.toString())) {
+      return false;
+    }
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    const puzzleStringWithPlaceholder =
+      this.insertPlaceholderIntoPuzzleString(puzzleString, row, column);
+    const puzzleColumnWithPlaceholder = [];
+    for (let r = 1; r <= 9; r++) {
+      puzzleColumnWithPlaceholder.push(
+        puzzleStringWithPlaceholder.charAt((r - 1) * 9 + (column - 1))
+      );
+    }
+    if (puzzleColumnWithPlaceholder.includes(value.toString())) {
+      return false;
+    }
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    const puzzleStringWithPlaceholder =
+      this.insertPlaceholderIntoPuzzleString(puzzleString, row, column);
+    const firstRowOfRegion = (Math.floor((row - 1) / 3) * 3) + 1;
+    const firstColumnOfRegion = (Math.floor((column - 1) / 3) * 3) + 1;
+    let puzzleRegionWithPlaceholder = [];
+    for (let r = 1; r <= 3; r++) {
+      puzzleRegionWithPlaceholder = puzzleRegionWithPlaceholder.concat(
+        [...(puzzleStringWithPlaceholder.substr(
+          ((firstRowOfRegion - 1) + (r - 1)) * 9 + (firstColumnOfRegion - 1), 3)
+        )]
+      );
+    }
+    if (puzzleRegionWithPlaceholder.includes(value.toString())) {
+      return false;
+    }
+    return true;
+  }
 
+  checkPlacement(puzzleString, row, column, value) {
+    return (this.checkRowPlacement(puzzleString, row, column, value) &&
+            this.checkColPlacement(puzzleString, row, column, value) &&
+            this.checkRegionPlacement(puzzleString, row, column, value))
+  }
+
+  getPlaceholdersWithValues(puzzleString) {
+    const placeholders = [];
+    for (let i = 0; i < 81; i++) {
+      if (puzzleString.charAt(i) == '.') {
+        const row = Math.floor(i / 9) + 1;
+        const column = (i % 9) + 1;
+        const values = [];
+        for (let v = 1; v <= 9; v++) {
+          if (this.checkPlacement(puzzleString, row, column, v)) {
+            values.push(v);
+          }
+        }
+        placeholders.push({
+          row: row,
+          column: column,
+          value: '.',
+          values: values
+        });
+      }
+    }
+    return placeholders;
+  }
+  
+  getSolution(puzzleString, placeholders) {
+    // placeholders == this.getPlaceholdersWithValues(puzzleString)
+    if (placeholders.length == 0) {
+      return {
+        solution: puzzleString // placeholders resolved
+      };
+    }
+    if (placeholders[0].values.length == 0) {
+      return {
+        error: 'Puzzle cannot be solved' // no values to try
+      };
+    }
+    if (this.checkPlacement(
+          puzzleString, placeholders[0].row, placeholders[0].column,
+          placeholders[0].values[0])) {
+      const result = this.getSolution(
+         puzzleString.replace('.', placeholders[0].values.shift()),
+         placeholders.slice(1));
+      if (result.hasOwnProperty('solution')) {
+        return result; // solution found
+      }
+    }
+    return this.getSolution(puzzleString, placeholders); // try next value
   }
 
   solve(puzzleString) {
-    
+    const result = this.validate(puzzleString);
+    if (result.valid == false) {
+      return {
+        error: result.error
+      };
+    } 
+    for (let r = 1; r <= 9; r++) {
+      for (let c = 1; c <= 9; c++) {
+        const value = puzzleString.charAt((r - 1) * 9 + (c - 1));
+        if (value != '.' && this.checkPlacement(puzzleString, r, c, value) == false) {
+          return {
+            error: 'Puzzle cannot be solved' // invalid puzzleString
+          };
+        }
+      }
+    }
+    const placeholders = this.getPlaceholdersWithValues(puzzleString);
+    return this.getSolution(puzzleString, placeholders);
   }
+
 }
 
 module.exports = SudokuSolver;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,106 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('Unit Tests', () => {
+
+  const validPuzzle = '1.34567894567891.37891.3456.34567891567891.34891.3456734567891.67891.34591.345678';
+  const validPuzzleSolution = validPuzzle.replace(/\./g, "2");
+
+  suite('SudokuSolver.validate', () => {
+    test('01. Valid puzzle', () => {
+      // assert.isTrue(solver.validate(validPuzzle));
+      const result = solver.validate(validPuzzle);
+      assert.isObject(result);
+      assert.propertyVal(result, 'valid', true);
+    });
+    test('02. Puzzle with invalid characters', () => {
+      // assert.isFalse(solver.validate(validPuzzle.replace(/1/g, "X")));
+      const error = solver.validate(validPuzzle.replace(/1/g, "X"));
+      assert.isObject(error);
+      assert.propertyVal(error, 'valid', false);
+      assert.propertyVal(error, 'error', 'Invalid characters in puzzle');
+    });
+    test('03. Puzzle with invalid length', () => {
+      // assert.isFalse(solver.validate(validPuzzle.replace(/1/g, "")));
+      const error = solver.validate(validPuzzle.replace(/1/g, ""));
+      assert.isObject(error);
+      assert.propertyVal(error, 'valid', false);
+      assert.propertyVal(error, 'error', 'Expected puzzle to be 81 characters long');
+    });
+  });
+
+  suite('SudokuSolver.checkRowPlacement', () => {
+    test('04. Valid row placement', () => {
+      assert.isTrue(solver.checkRowPlacement(validPuzzle, 1, 2, 2));
+    });
+    test('05. Invalid row placement', () => {
+      assert.isFalse(solver.checkRowPlacement(validPuzzle, 2, 8, 8), 'Row 2 already includes 8');
+    });
+  });
+
+  suite('SudokuSolver.checkColPlacement', () => {
+    test('06. Valid column placement', () => {
+      assert.isTrue(solver.checkColPlacement(validPuzzle, 1, 2, 2));
+    });
+    test('07. Invalid column placement', () => {
+      assert.isFalse(solver.checkColPlacement(validPuzzle, 2, 8, 8), 'Column 8 already includes 8');
+    });
+  });
+
+  suite('SudokuSolver.checkRegionPlacement', () => {
+    test('08. Valid region placement', () => {
+      assert.isTrue(solver.checkRegionPlacement(validPuzzle, 1, 2, 2));
+    });
+    test('09. Invalid region placement', () => {
+      assert.isFalse(solver.checkRegionPlacement(validPuzzle, 2, 8, 8), 'Region 1-3 already includes 8');
+    });
+  });
+
+  suite('SudokuSolver.solve', () => {
+    test('10. Valid (already solved) puzzle', () => {
+      const testPuzzle = validPuzzleSolution;
+      const solution = solver.solve(testPuzzle);
+      assert.isObject(solution);
+      assert.propertyVal(solution, 'solution', validPuzzleSolution);
+    });
+    test('11a. Puzzle with invalid characters', () => {
+      const testPuzzle = validPuzzle.replace(/1/g, "X");
+      const error = solver.solve(testPuzzle);
+      assert.isObject(error);
+      assert.propertyVal(error, 'error', 'Invalid characters in puzzle');
+    });
+    test('11b. Puzzle with invalid length', () => {
+      const testPuzzle = validPuzzle.replace(/1/g, "");
+      const error = solver.solve(testPuzzle);
+      assert.isObject(error);
+      assert.propertyVal(error, 'error', 'Expected puzzle to be 81 characters long');
+    });
+    test('11c. Puzzle with invalid row', () => {
+      const testPuzzle = '.........113456789...............................................................';
+      const error = solver.solve(testPuzzle);
+      assert.isObject(error);
+      assert.propertyVal(error, 'error', 'Puzzle cannot be solved');
+    });
+    test('11d. Puzzle with invalid column', () => {
+      const testPuzzle = '.1........1........3........4........5........6........7........8........9.......';
+      const error = solver.solve(testPuzzle);
+      assert.isObject(error);
+      assert.propertyVal(error, 'error', 'Puzzle cannot be solved');
+    });
+    test('11e. Puzzle with invalid region', () => {
+      const testPuzzle = '...113......456......789.........................................................';
+      const error = solver.solve(testPuzzle);
+      assert.isObject(error);
+      assert.propertyVal(error, 'error', 'Puzzle cannot be solved');
+    });
+    test('12. Solve incomplete puzzle', () => {
+      const testPuzzle = validPuzzle;
+      const solution = solver.solve(testPuzzle);
+      assert.isObject(solution);
+      assert.propertyVal(solution, 'solution', validPuzzleSolution);
+    });
+  });
 
 });
